d29fa78161425c4cb295db684aa35065ece7290e
Oderystella <ddott59@gmail.com>
2021-06-08 00:17:31 +0900

    fix: renaming of Place.like_comments, changing author field
    - place -> like_place
    - change OneToOneField to ForeignKey of author in Place

--- place/models.py
+++ place/models.py
@@ -17,7 +17,7 @@ class Tag(models.Model):
 
 class Place(BaseField):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='place')
-    author = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='place')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='place')
     name = models.CharField(max_length=64)
     location = models.CharField(max_length=64)
     memo = models.TextField()
@@ -25,7 +25,7 @@ class Place(BaseField):
     additional_info = models.CharField(max_length=200)
     stars = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     tags = models.ManyToManyField(Tag, blank=True, related_name='place')
-    like_comments = models.ManyToManyField(User, blank=True, related_name='place')
+    like_comments = models.ManyToManyField(User, blank=True, related_name='like_place')
 
     def __str__(self):
         return self.name
